16bda64417dd779b26013dda7fa01ac09a650234
NmrRaihan <nmraihan007@gmail.com>
2026-02-05 20:04:16 +0600

    Make student registration endpoint public (no authentication required)

--- backend/src/routes/studentRoutes.js
+++ backend/src/routes/studentRoutes.js
@@ -10,10 +10,10 @@ const { protect } = require('../middleware/auth');
 
 // Public routes
 router.post('/login', loginStudent);
+router.post('/', createStudent); // Student registration should be public
 
 // Protected routes
 router.get('/', protect, getAllStudents);
 router.get('/:id', protect, getStudentById);
-router.post('/', protect, createStudent);
 
 module.exports = router;
